1322d5ce3093d89157e9c7fec2988a21a55dc446
Davi José <davi.djsb@hotmail.com>
2026-09-01 16:50:06 -0300

    Resolução quase completa da questão 1 da lista de Threads (somas dos valores das threads já funcionais)

---
+++ Projeto Threads/Q1/Q1_threads.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define N 5 // Número de threads;
+#define NUM_ARRAY 10 // Número de elementos do vetor;
+
+typedef struct{
+    int pos_init;
+    int partition;
+    int* data;
+    int partial;
+} _subarray;
+
+void* Sum(void* subarray){
+    _subarray* array = (_subarray*) subarray;
+    int pos_init = array->pos_init;
+    int partition = array->partition;
+
+    for (int j = pos_init; j < (pos_init + partition); j++){
+        array->partial += array->data[j];
+    }
+
+    pthread_exit((void*) array);
+}
+
+int* SetToZero(int* array){
+    for (int i = 0; i < NUM_ARRAY; i++){
+        array[i] = 5;
+    }
+
+    return array;
+}
+
+//No Scheduler, vamos designar para cada uma das threads quantos elementos elas vão processar;
+int* Scheduler(){ 
+    float partition = NUM_ARRAY / N;
+    int i = 0;
+    int nat_partition = partition;
+
+    int* schedule = NULL;
+
+    if (!(schedule = (int*) malloc(N * sizeof(int)))){
+        printf("Erro de alocação de memória no scheduler!\n");
+        return NULL;
+    }
+
+    while (i < N){
+        if ((i == (N - 1)) && (nat_partition < partition)) schedule[i] = nat_partition + 1;
+        else schedule[i] = nat_partition;
+
+        i++;
+    }
+
+    return schedule;
+}
+
+int main(){
+    pthread_t thread_list[N];
+    int rc;
+    int* numeric_array = NULL;
+    int pos = 0;
+    int* schedule = Scheduler();
+    int part_results[N];
+    int total = 0;
+    _subarray __subarray[N];
+
+    if (!(numeric_array = (int*) malloc(NUM_ARRAY * sizeof(int)))){
+        printf("Erro de alocação de memória para array numérica!\n");
+        exit(1);
+    }
+
+    numeric_array = SetToZero(numeric_array);
+
+    for (int i = 0; i < N; i++){ //mudar i para N
+        __subarray[i].data = numeric_array;
+        __subarray[i].partition = schedule[i];
+        __subarray[i].pos_init = pos;
+        __subarray[i].partial = 0;
+
+        rc = pthread_create(&thread_list[i], NULL, Sum, (void*) &__subarray[i]);
+        if (rc){
+            printf("Erro de criação da thread; Código de erro: %d\n", rc);
+            exit(1);
+        }
+
+        pos += schedule[i];
+    }
+
+    for (int j = 0; j < N; j++){ //MUDAR J PARA N
+        _subarray* _array;
+        
+        rc = pthread_join(thread_list[j], (void**) &_array);
+        if (rc){
+            printf("Erro de join da thread; Código de erro: %d\n", rc);
+            exit(1);
+        }
+
+        total += _array->partial;
+    }
+
+    printf("Total: %d\n", total);
+}
