eec5dddc95a07ca061fccee04a720b2a31f742b7
Matheus Vasconcelos <matheussv.0704@gmail.com>
2026-09-17 17:57:20 -0300

    Implementação da escrita e da leitura no array

--- Projeto Threads/Q3/Q3_threads.c
+++ Projeto Threads/Q3/Q3_threads.c
@@ -8,6 +8,8 @@
 
 int elemento = 0; // Variável que vai preencher as posições do array.
 int array[NUM_WRITE]; // Array que será usado pelas threads de escrita
+int escritor_ativo = 0; // Variável de 0 ou 1 que informa se há um escritor atualmente escrevendo ou não
+int leitores_ativos = 0; // Quantidade de leitores atualmente sendo utilizados
 
 pthread_mutex_t mutex;
 pthread_cond_t cond_parada; // Variável de condição para bloquear as threads de leitura enquanto uma escrita estiver em execução
@@ -15,20 +17,58 @@ pthread_cond_t cond_parada; // Variável de condição para bloquear as threads 
 // ainda não tiverem sido executadas por inteiro. Ao final das últimas (as threads escritoras), haverá um pthread_cond_broadcast
 // para liberar todas as threas de leitura simultaneamente
 
-void* write_rotina(){
+void* write_rotina(void *arg){
+    int index = *(int*) arg; // Retorna o tipo para int
+    free(arg);
+
     while(1){
+        pthread_mutex_lock(&mutex); // Trava o mutex ao entrar na thread
 
+        while(escritor_ativo || leitores_ativos > 0){ // Enquanto houver uma thread de escrita ainda ativa ou uma thread lendo
+            pthread_cond_wait(&cond_parada, &mutex);
+        }
 
-        elemento++;
-        pthread_cond_broadcast(&cond_parada); // Libera todas as threas de leitura
+        escritor_ativo = 1; // Ativa a nova thread de escrita
+        pthread_mutex_unlock(&mutex);
+
+        elemento += 3; // Incrementa de 3 em 3 os valores
+        array[index] = elemento;
+
+        pthread_mutex_lock(&mutex);
+        escritor_ativo = 0;
+        pthread_cond_broadcast(&cond_parada); // Libera todas as threads em wait
+        pthread_mutex_unlock(&mutex);
+        sleep(1);
     }
 }
 
 
-void* read_rotina(){
+void* read_rotina(void *arg){
+    int index = *(int*) arg; // Retorna o tipo para int
+    free(arg);
+
     while(1){
-        pthread_cond_wait(&cond_parada ,&mutex); // Aguarda a finalização das threads de escritura
-        // Inserir leitura
+        pthread_mutex_lock(&mutex);
+        while(escritor_ativo){
+            pthread_cond_wait(&cond_parada ,&mutex); // Aguarda a finalização das threads de escritura
+        }
+
+        leitores_ativos++;
+        pthread_mutex_unlock(&mutex);
+
+        printf("Leitura %d: ", index-4);
+        for(int i = 0; i < NUM_WRITE; i++){ // Imprime os elementos dos arrays
+            printf("%d ", array[i]);
+        }
+        printf("\n\n");
+
+        pthread_mutex_lock(&mutex);
+        leitores_ativos--;
+        if(leitores_ativos == 0){
+            pthread_cond_broadcast(&cond_parada); // Libera as threads escritoras
+        }
+        pthread_mutex_unlock(&mutex);
+        sleep(1);
     }
 }
 
@@ -39,15 +79,17 @@ int main(int argc, char** argv){
     pthread_cond_init(&cond_parada, NULL);
 
     for(int i = 0; i < NUM_READ + NUM_WRITE; i++){
+        int* number = malloc(sizeof(int));
+        *number = i; // Cada thread escritora vai escrever na posição correspondente em que foi criada
         if(i < NUM_WRITE){ // Cria 5 threads escritoras
-            if(pthread_create(threads + i, NULL, &write_rotina, NULL)){ // Adicionar argumentos para mandar p/ função
+            if(pthread_create(threads + i, NULL, &write_rotina, number)){
                 perror("Erro ao criar as threads escritoras");
                 return 1;
             }
         }
 
         else{ // Cria 3 threads leitoras
-            if(pthread_create(threads + i, NULL, &read_rotina, NULL)){ // Adicionar argumentos p/ mandar p/ função
+            if(pthread_create(threads + i, NULL, &read_rotina, number)){
                 perror("Erro ao criar as threads leitoras");
                 return 2;
             }
