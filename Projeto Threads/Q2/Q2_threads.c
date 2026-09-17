0776599ece926d4735b3bad9f2e3a15ea98f1a08
Davi José <davi.djsb@hotmail.com>
2026-09-17 20:19:45 -0300

    Resolução questão 2 (ainda falta comentar)

--- Projeto Threads/Q2/Q2_threads.c
+++ Projeto Threads/Q2/Q2_threads.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <string.h>
 
-#define N 2        //Número de arquivos;
-#define L 2        //Número de linhas por arquivo;
+#define N 5        //Número de arquivos e número de threads;
+#define L 5        //Número de linhas por arquivo;
 #define L_TABELA 7 //Número de linhas da tabela;
 
 pthread_mutex_t lock[L_TABELA];
@@ -16,41 +17,23 @@ typedef struct{
 
 typedef struct{
     int file_id; //Identificador do arquivo;
-    pthread_t leitora; //Thread que lê o arquivo;
-    pthread_t escritora; //Thread que escreve na tabela;
     paciente _lista_pacientes[L];
+} leitura_thread;
+
+typedef struct{
+    pthread_t leitora;
+    pthread_t escritora;
 } le_thread;
 
-void* LerArquivo(void* thread_file){
-    char buffer[100];
-    FILE* arquivo = NULL;
-    char nome_arquivo[50];
-    le_thread* leitura = (le_thread*) thread_file;
-    int i = 0; // Variável para iterar em cima dos elementos da lista de pacientes;
-
-    sprintf(nome_arquivo, "arquivo_%d.txt\n", leitura->file_id);
-
-    if(!(arquivo = fopen(nome_arquivo, "r"))){ // Checagem de erro caso o arquivo não abra;
-        perror("Problema ao abrir o arquivo");
-        exit(1);
-    }
-
-    while (!((fgets(buffer, sizeof(buffer), arquivo)) == NULL)){ // Loop de leitura de cada uma das linhas dos arquivos;
-        if(!(sscanf(buffer, "%s %d", leitura->_lista_pacientes[i].nome, &leitura->_lista_pacientes[i].consultorio))){ // Armazenamento e checagem de erro;
-            printf("Formato de linha inválido\n");
-        }
-        
-        i++;
-    }
-
-    fclose(arquivo);
-    pthread_exit((void*) leitura);
-
-}
-
-void GerarTabela(){
+void InitMutex(){
+    int rc;
+    
     for (int i = 0; i < L_TABELA; i++){
-        printf("\033[37;%dmPaciente --- Consultorio %d\n\033[0m", (i+40), (i+1));
+        rc = pthread_mutex_init(&lock[i], NULL);
+        if (rc){
+            printf("Erro ao criar MUTEX\n");
+            exit(1);
+        }
     }
 }
 
@@ -60,36 +43,102 @@ void DestroyMutex(){
     }
 }
 
-void InitMutex(){
-    int rc;
-    
+void GerarTabela(){
+    printf("\033[2J");
+    printf("\033[H");
     for (int i = 0; i < L_TABELA; i++){
-        rc = pthread_mutex_init(&lock[i], NULL);
-        if (rc){
-            DestroyMutex();
-            printf("Erro ao criar MUTEX\n");
-            exit(1);
-        }
+        printf("\033[37;4%dmPaciente --- Consultorio %d\n\033[0m", i, (i+1));
     }
 }
 
-void* Escrita(void* thread_file){
-    le_thread* escrita = (le_thread*) thread_file;
+void* LerArquivo(void* thread_file){
+    int i = 0; // Variável para iterar em cima dos elementos da lista de pacientes;
+    char buffer[100];
+    FILE* arquivo = NULL;
+    char nome_arquivo[50];
+
+    leitura_thread* leitura = malloc(sizeof(leitura_thread));
+    if (leitura == NULL){
+        printf("Erro de alocacao de memoria para alocacao do arquivo\n");
+        exit(1);
+    }
+
+    leitura->file_id = *(int*) thread_file;
+
+    sprintf(nome_arquivo, "arquivo_%d.txt", leitura->file_id+1);
+
+    if(!(arquivo = fopen(nome_arquivo, "r"))){ // Checagem de erro caso o arquivo não abra;
+        printf("Problema ao abrir o arquivo %d\n", leitura->file_id+1);
+        exit(1);
+    }
+
+    while (!((fgets(buffer, sizeof(buffer), arquivo)) == NULL)){ // Loop de leitura de cada uma das linhas dos arquivos;
+        sscanf(buffer, "%s %d", leitura->_lista_pacientes[i].nome, &leitura->_lista_pacientes[i].consultorio);
+        i++;
+    }
+
+    fclose(arquivo);
+    pthread_exit((void*) leitura);
+}
+
+void* Escrita(void* thread_leitora){
+    int rc;
+    int consultorio;
+    int mutex_index;
+
+    leitura_thread* resultado_leitura = NULL;
+
+    pthread_t* leitora = (pthread_t*) thread_leitora;
+    
+    rc = pthread_join(*leitora, (void**) &resultado_leitura);
+    if (rc){
+        printf("Erro de join da thread de escrita\n");
+        exit(1);
+    }
 
     for (int i = 0; i < L; i++){ //L é tanto o número de linhas por arquivo, como também o número de pacientes por arquivo;
+        consultorio = resultado_leitura->_lista_pacientes[i].consultorio;
+        mutex_index = consultorio - 1;
         
+        pthread_mutex_lock(&lock[mutex_index]);
+        printf("\033[%d;1H\033[2K", consultorio);
+        printf("\033[37;4%dmPaciente %s Consultorio %d\033[0m\033[%d;1H\n",(consultorio-1)%8, resultado_leitura->_lista_pacientes[i].nome, consultorio, L_TABELA+2);
+        sleep(5);
+        pthread_mutex_unlock(&lock[mutex_index]);
     }
+    printf("\033[8;1H");
+
+    free(resultado_leitura);
+    pthread_exit(NULL);
 }
 
-void PrintTabela(le_thread* thread_file_list){
-    for (int i = 0; i < N; i++){}
-}
-
-int main(int argc, char** argv){
+int main(){
+    int rc;
+    int indices[N];
+    le_thread le_thread_list[N];
     GerarTabela();
     InitMutex();
 
+    for (int i = 0; i < N; i++){
+        indices[i] = i;
+        rc = pthread_create(&le_thread_list[i].leitora, NULL, LerArquivo, (void*) &indices[i]);
+        
+        if (rc){
+            printf("Erro na criação da thread de leitura %d\n", i+1);
+            exit(1);
+        }
 
+        rc = pthread_create(&le_thread_list[i].escritora, NULL, Escrita, (void*) &le_thread_list[i].leitora);
+
+        if (rc){
+            printf("Erro na criacao da thread de escrita %d\n", i+1);
+            exit(1);
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        pthread_join(le_thread_list[i].escritora, NULL);
+    }
 
     DestroyMutex();
     return 0;
