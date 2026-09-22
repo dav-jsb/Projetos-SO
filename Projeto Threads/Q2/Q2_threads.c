a41a8ead8f5fb1214ace864fad22da1fbcaaf524
Davi José <davi.djsb@hotmail.com>
2026-09-22 00:17:15 -0300

    Comentários das questões 2 e 4

--- Projeto Threads/Q2/Q2_threads.c
+++ Projeto Threads/Q2/Q2_threads.c
@@ -4,18 +4,23 @@
 #include <unistd.h>
 #include <string.h>
 
+//Nosso algoritmo se baseou na criação de uma função simples de leitura que retornaria uma lista de pacientes para cada thread, a qual imprimiria na tabela esses pacientes;
+//Utilizamos das variáveis mutex para que cada linha fosse travada por 5 segundos para escrita e demonstração do paciente na tabela;
+//Utilizamos a função sleep da unistd para definir esse tempo de espera;
+
 #define N 5 //Número de arquivos e threads;
 #define L 3 //Número de linhas;
 #define L_TABELA 7 //Número de linhas da tabela;
+#define SLEEP 5 //Tempo de exibição da linha na tabela;
 
-typedef struct{
+typedef struct{ //Struct responável por armazenar o nome e o consultório de cada pessoa lida no arquivo;
     char nome[50];
     int consultorio;
 }paciente;
 
-pthread_mutex_t lock[L_TABELA];
+pthread_mutex_t lock[L_TABELA]; //Array de mutex para travamento de cada linha da tabela;
 
-void InitMutex(){
+void InitMutex(){//Função de inicialização dos Mutex;
     for (int i = 0; i < L_TABELA; i++){
         if (pthread_mutex_init(&lock[i], NULL)){
             printf("Erro ao inicializar mutex\n");
@@ -24,7 +29,7 @@ void InitMutex(){
     }
 }
 
-void DestroyMutex(){
+void DestroyMutex(){//Função de destruição dos mutex;
     for (int i = 0; i < L_TABELA; i++){
         pthread_mutex_destroy(&lock[i]);
     }
@@ -42,14 +47,14 @@ void Leitura(paciente* lista, int id_file){
         exit(1);
     }
 
-    while (!(fgets(buffer, sizeof(buffer), file) == NULL)){
+    while (!(fgets(buffer, sizeof(buffer), file) == NULL)){ //Aqui, utilizamos o fgets para ler linha por linha e depois o sscanf para extrair os valores desejados (considerando principalmente que a entrada é amigável);
         sscanf(buffer, "%s %d", lista[iterator].nome, &lista[iterator].consultorio);
         iterator++;
     }
     fclose(file);
 }
 
-void GenerateTabela(){
+void GenerateTabela(){//Aqui, limpamos a tela, redirecionamos o ponteiro para o início do terminal e depois printamos a linha base (paciente --- consultório x);
     printf("\033[2J");
     printf("\033[H");
     for (int i = 0; i < L_TABELA; i++){
@@ -61,16 +66,16 @@ void GenerateTabela(){
 void* WriteTabela(void* file_id){
     int id = *(int*) file_id;
     paciente lista[L];
-    Leitura(lista, id+1);
+    Leitura(lista, id+1); //Passamos a lista de paciente para a função de leitura para que possamos ter a lista de pacientes do arquivo;
 
     for (int i = 0; i < L; i++){
-        pthread_mutex_lock(&lock[i]);
+        pthread_mutex_lock(&lock[i]); //Utilizamos o mutex para travar a linha que necessitamos;
         printf("\033[%d;1H", lista[i].consultorio);
         printf("\033[2K");
-        printf("\033[37;4%dmPaciente %s Consultorio %d\033[0m\n\033[1A", (lista[i].consultorio-1)%8, lista[i].nome, lista[i].consultorio);
+        printf("\033[37;4%dmPaciente %s Consultorio %d\033[0m\n\033[1A", (lista[i].consultorio-1)%8, lista[i].nome, lista[i].consultorio); //Print ajustado de cada paciente na tabela;
         //printf("\033[%d;1H", L_TABELA+1);
-        sleep(5);
-        pthread_mutex_unlock(&lock[i]);
+        sleep(SLEEP); //Sleep de 5 segundos para visualização do paciente na tabela;
+        pthread_mutex_unlock(&lock[i]); //Liberação da linha utilizada;
     }
     pthread_exit(NULL);
 }
@@ -81,11 +86,11 @@ int main(){
     int file_ids[N];
     int rc;
 
-    InitMutex();
-    GenerateTabela();
+    InitMutex(); //Inicialização dos mutex;
+    GenerateTabela(); //Geração da tabela base;
     
     while (iterator < N){
-        file_ids[iterator] = iterator;
+        file_ids[iterator] = iterator; //Definição dos ids de cada arquivo;
         if (rc = pthread_create(&thread_list[iterator], NULL, WriteTabela, (void*) &file_ids[iterator])){
             printf("Erro ao criar threads\n");
             exit(1);
@@ -93,15 +98,15 @@ int main(){
         iterator++;
     }
     
-    for (int i = 0; i < N; i++){
+    for (int i = 0; i < N; i++){ //Utilização do join para aguardar que todas as threads terminem suas funções;
         if (rc = pthread_join(thread_list[i], NULL)){
             printf("Erro de espera de thread\n");
             exit(1);
         }
     }
 
-    printf("\033[%d;1H", L_TABELA+2);
+    printf("\033[%d;1H", L_TABELA+2); //Mover o ponteiro para 2 espaços abaixo da tabela;
 
-    DestroyMutex();
-    pthread_exit(NULL);
+    DestroyMutex(); //Destruição dos mutex;
+    pthread_exit(NULL); //Liberação das threads;
 }
--- Projeto Threads/Q4/Q4_threads.c
+++ Projeto Threads/Q4/Q4_threads.c
@@ -3,20 +3,26 @@
 #include <pthread.h>
 #include <time.h>
 
+//Em resumo, nossa implementação utilizou-se de um escalonador para definir que variáveis cada thread ficaria responsável;
+//Além disso, utilizamos as variáveis de barreira para obrigar que os valores das variáveis só fossem atualizados sincronamente (permitindo o pleno funcionamento do método de Jacobi)
+//Dentro da implementação de Jacobi, também utilizamos de uma array que armazenaria os valores temporários de cada um dos resultados de cada iteração (só atualizando sincronamente);
+
 #define N 10 //Número de threads;
 #define I 4 //Número de incógnitas;
 #define P 10 //Número de iterações;
 
 typedef struct{
-    int pos_init;
-    int partition;
-} thread_data;
+    int pos_init; //Posicão inicial na array de variáveis;
+    int partition; //Quantidade de variáveis por thread;
+} thread_data; //Struct que contém a informação de quais variáveis cada thread vai ficar responsável;
 
-pthread_barrier_t barrier;
+pthread_barrier_t barrier; //Variável de barreira para implementação do algoritmo de Jacobi;
+//Sistema linear representado abaixo;
 float coeficients[I][I] = {{10, -1, 2, 0}, {-1, 11, -1 , 3}, {2, -1, 10, -1}, {0, 3, -1, 8}};
 float variable[I];
 float independent[I] = {6, 25, -11, 15};
 
+//Função para inicializar barreira conforme a quantidade de threads utilizadas;
 void InitBarrier(int count, pthread_barrier_t* barrier_var){
     int rc;
     if (rc = pthread_barrier_init(barrier_var, NULL, count)){
@@ -25,71 +31,71 @@ void InitBarrier(int count, pthread_barrier_t* barrier_var){
     }
 }
 
+//Função para destruir a variável de barreira;
 void DestroyBarrier(pthread_barrier_t* barrier_var){
     pthread_barrier_destroy(barrier_var);
 }
 
-int Scheduler(int* schedule){
+int Scheduler(int* schedule){ //Escalonador responsável por definir com quais variáveis cada thread vai ficar responsável e retornar quantas threads serão utilizadas;
     int remainder = I%N;
     int partition = I/N;
     int count = N;
 
     for (int i = 0; i < I; i++){
-        schedule[i] = partition;
+        schedule[i] = partition; //Divide a partição para cada uma das threads e depois distribui o resto;
         if (remainder > 0){ 
             schedule[i]++;
             remainder--;
         }
     }
 
-    if (!partition) count = I;
+    if (!partition) count = I;//O escalonador também define quantas threads serão necessárias para a resolução do sistema;
     return count;
 }
 
-void SetVariable(){
+void SetVariable(){ //Função para setar toda a array de variáveis para 1;
     for (int i = 0; i < I; i++) variable[i] = 1;
 }
 
-void* Jacobi(void* thread_file){
+void* Jacobi(void* thread_file){//Algoritmo de Jacobi implementado;
     thread_data data = *(thread_data*) thread_file;
-    int iter = P;
-    float sum_ax;
+    int iter = P; //Variável que armazena o número de iterações;
+    float sum_ax; //Soma de aij*xj com j diferente de i;
     int pos_init = data.pos_init;
     int partition = data.partition;
-
-    if (partition){
-        float temp_variable[partition];
-        while (iter > 0){
-            for (int i = 0; i < partition; i++){
-                sum_ax = 0;
-                for (int j = 0; j < I; j++){
-                    if (j != (pos_init + i)) sum_ax += coeficients[pos_init + i][j] * variable[j];
-                }
-                temp_variable[i] = (independent[pos_init + i] - sum_ax) / coeficients[pos_init+i][pos_init+i];
+    float temp_variable[partition]; //Array que armazena os resultados de cada iteração até que possa ser atualizado na array global;
+    
+    while (iter > 0){
+        for (int i = 0; i < partition; i++){
+            sum_ax = 0;
+            for (int j = 0; j < I; j++){
+                if (j != (pos_init + i)) sum_ax += coeficients[pos_init + i][j] * variable[j]; //Cálculo do sum_ax;
             }
-            iter--;
-            pthread_barrier_wait(&barrier);
-            for (int i = 0; i < partition; i++){
-                variable[pos_init + i] = temp_variable[i];
-            }
-            pthread_barrier_wait(&barrier);
+            temp_variable[i] = (independent[pos_init + i] - sum_ax) / coeficients[pos_init+i][pos_init+i]; //Resultado para a atual iteração do valor de xi;
         }
+        iter--;
+        pthread_barrier_wait(&barrier); //Variável de barreira para aguardar que todas as variáveis tenham calculado sua iteração;
+        for (int i = 0; i < partition; i++){
+            variable[pos_init + i] = temp_variable[i]; //Mudança da variável na array global;
+        }
+        pthread_barrier_wait(&barrier); //Variável de barreira para aguardar que todas as threads tenham alterado a array global em determinada iteração;
     }
+    
     pthread_exit(NULL);
 }
 
 int main(){
-    thread_data data_list[N];
-    pthread_t thread_list[N];
+    thread_data data_list[N]; //Lista de informações definidoras das variáveis para cada thread;
+    pthread_t thread_list[N]; //Lista de threads propriamente ditas;
     int schedule[N];
     int count, rc, jc;
     int pos = 0;
     
     SetVariable();
-    count = Scheduler(schedule);
-    InitBarrier(count, &barrier);
+    count = Scheduler(schedule); 
+    InitBarrier(count, &barrier); //Definição da quantidade de threads com base no count do escalonador;
     
-    for (int i = 0; i < count; i++){
+    for (int i = 0; i < count; i++){ //Criação de cada thread;
         data_list[i].partition = schedule[i];
         data_list[i].pos_init = pos;
         pos += data_list[i].partition;
@@ -101,7 +107,7 @@ int main(){
 
     }
 
-    for (int i = 0; i < count; i++){
+    for (int i = 0; i < count; i++){ //Loop responsável por aguardar o término de cada thread;
         jc = pthread_join(thread_list[i], NULL);
         if (jc){
             printf("Erro de join na thread\n");
@@ -114,6 +120,7 @@ int main(){
     }
     printf("\n");
 
+    //Destruição da variável de barreira e liberação das threads;
     DestroyBarrier(&barrier);
     pthread_exit(NULL);
 }
