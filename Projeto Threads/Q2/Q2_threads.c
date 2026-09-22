136bae8b079c5b55ed1ecc52fbc0144ea7d04445
Davi José <davi.djsb@hotmail.com>
2026-09-21 22:10:34 -0300

    Resolução da Q4 (ainda falta comentar)

--- Projeto Threads/Q2/Q2_threads.c
+++ Projeto Threads/Q2/Q2_threads.c
@@ -1,37 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <pthread.h>
+#include <unistd.h>
 #include <string.h>
 
-#define N 5        //Número de arquivos e número de threads;
-#define L 5        //Número de linhas por arquivo;
+#define N 5 //Número de arquivos e threads;
+#define L 3 //Número de linhas;
 #define L_TABELA 7 //Número de linhas da tabela;
+
+typedef struct{
+    char nome[50];
+    int consultorio;
+}paciente;
 
 pthread_mutex_t lock[L_TABELA];
 
-typedef struct{ 
-    char nome[50];
-    int consultorio;
-} paciente;
-
-typedef struct{
-    int file_id; //Identificador do arquivo;
-    paciente _lista_pacientes[L];
-} leitura_thread;
-
-typedef struct{
-    pthread_t leitora;
-    pthread_t escritora;
-} le_thread;
-
 void InitMutex(){
-    int rc;
-    
     for (int i = 0; i < L_TABELA; i++){
-        rc = pthread_mutex_init(&lock[i], NULL);
-        if (rc){
-            printf("Erro ao criar MUTEX\n");
+        if (pthread_mutex_init(&lock[i], NULL)){
+            printf("Erro ao inicializar mutex\n");
             exit(1);
         }
     }
@@ -43,103 +30,78 @@ void DestroyMutex(){
     }
 }
 
-void GerarTabela(){
+void Leitura(paciente* lista, int id_file){
+    FILE* file = NULL;
+    char file_name[50];
+    sprintf(file_name, "arquivo_%d.txt", id_file);
+    char buffer[50];
+    int iterator = 0;
+
+    if (!(file = fopen(file_name, "r"))){
+        printf("Erro de abertura de arquivo\n");
+        exit(1);
+    }
+
+    while (!(fgets(buffer, sizeof(buffer), file) == NULL)){
+        sscanf(buffer, "%s %d", lista[iterator].nome, &lista[iterator].consultorio);
+        iterator++;
+    }
+    fclose(file);
+}
+
+void GenerateTabela(){
     printf("\033[2J");
     printf("\033[H");
     for (int i = 0; i < L_TABELA; i++){
-        printf("\033[37;4%dmPaciente --- Consultorio %d\n\033[0m", i, (i+1));
+        printf("\033[37;4%dmPaciente --- Consultorio %d\n\033[0m", i%8, (i+1));
     }
+    printf("\033[%d;1H", L_TABELA);
 }
 
-void* LerArquivo(void* thread_file){
-    int i = 0; // Variável para iterar em cima dos elementos da lista de pacientes;
-    char buffer[100];
-    FILE* arquivo = NULL;
-    char nome_arquivo[50];
+void* WriteTabela(void* file_id){
+    int id = *(int*) file_id;
+    paciente lista[L];
+    Leitura(lista, id+1);
 
-    leitura_thread* leitura = malloc(sizeof(leitura_thread));
-    if (leitura == NULL){
-        printf("Erro de alocacao de memoria para alocacao do arquivo\n");
-        exit(1);
-    }
-
-    leitura->file_id = *(int*) thread_file;
-
-    sprintf(nome_arquivo, "arquivo_%d.txt", leitura->file_id+1);
-
-    if(!(arquivo = fopen(nome_arquivo, "r"))){ // Checagem de erro caso o arquivo não abra;
-        printf("Problema ao abrir o arquivo %d\n", leitura->file_id+1);
-        exit(1);
-    }
-
-    while (!((fgets(buffer, sizeof(buffer), arquivo)) == NULL)){ // Loop de leitura de cada uma das linhas dos arquivos;
-        sscanf(buffer, "%s %d", leitura->_lista_pacientes[i].nome, &leitura->_lista_pacientes[i].consultorio);
-        i++;
-    }
-
-    fclose(arquivo);
-    pthread_exit((void*) leitura);
-}
-
-void* Escrita(void* thread_leitora){
-    int rc;
-    int consultorio;
-    int mutex_index;
-
-    leitura_thread* resultado_leitura = NULL;
-
-    pthread_t* leitora = (pthread_t*) thread_leitora;
-    
-    rc = pthread_join(*leitora, (void**) &resultado_leitura);
-    if (rc){
-        printf("Erro de join da thread de escrita\n");
-        exit(1);
-    }
-
-    for (int i = 0; i < L; i++){ //L é tanto o número de linhas por arquivo, como também o número de pacientes por arquivo;
-        consultorio = resultado_leitura->_lista_pacientes[i].consultorio;
-        mutex_index = consultorio - 1;
-        
-        pthread_mutex_lock(&lock[mutex_index]);
-        printf("\033[%d;1H\033[2K", consultorio);
-        printf("\033[37;4%dmPaciente %s Consultorio %d\033[0m\033[%d;1H\n",(consultorio-1)%8, resultado_leitura->_lista_pacientes[i].nome, consultorio, L_TABELA+2);
+    for (int i = 0; i < L; i++){
+        pthread_mutex_lock(&lock[i]);
+        printf("\033[%d;1H", lista[i].consultorio);
+        printf("\033[2K");
+        printf("\033[37;4%dmPaciente %s Consultorio %d\033[0m\n\033[1A", (lista[i].consultorio-1)%8, lista[i].nome, lista[i].consultorio);
+        //printf("\033[%d;1H", L_TABELA+1);
         sleep(5);
-        pthread_mutex_unlock(&lock[mutex_index]);
+        pthread_mutex_unlock(&lock[i]);
     }
-    printf("\033[8;1H");
-
-    free(resultado_leitura);
     pthread_exit(NULL);
 }
 
 int main(){
+    int iterator = 0;
+    pthread_t thread_list[N];
+    int file_ids[N];
     int rc;
-    int indices[N];
-    le_thread le_thread_list[N];
-    GerarTabela();
+
     InitMutex();
-
+    GenerateTabela();
+    
+    while (iterator < N){
+        file_ids[iterator] = iterator;
+        if (rc = pthread_create(&thread_list[iterator], NULL, WriteTabela, (void*) &file_ids[iterator])){
+            printf("Erro ao criar threads\n");
+            exit(1);
+        }
+        iterator++;
+    }
+    
     for (int i = 0; i < N; i++){
-        indices[i] = i;
-        rc = pthread_create(&le_thread_list[i].leitora, NULL, LerArquivo, (void*) &indices[i]);
-        
-        if (rc){
-            printf("Erro na criação da thread de leitura %d\n", i+1);
-            exit(1);
-        }
-
-        rc = pthread_create(&le_thread_list[i].escritora, NULL, Escrita, (void*) &le_thread_list[i].leitora);
-
-        if (rc){
-            printf("Erro na criacao da thread de escrita %d\n", i+1);
+        if (rc = pthread_join(thread_list[i], NULL)){
+            printf("Erro de espera de thread\n");
             exit(1);
         }
     }
 
-    for (int i = 0; i < N; i++) {
-        pthread_join(le_thread_list[i].escritora, NULL);
-    }
+    printf("\033[%d;1H", L_TABELA+2);
 
     DestroyMutex();
-    return 0;
+    pthread_exit(NULL);
 }
--- Projeto Threads/Q4/Q4_threads.c
+++ Projeto Threads/Q4/Q4_threads.c
@@ -1,1 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
 
+#define N 10 //Número de threads;
+#define I 4 //Número de incógnitas;
+#define P 10 //Número de iterações;
+
+typedef struct{
+    int pos_init;
+    int partition;
+} thread_data;
+
+pthread_barrier_t barrier;
+float coeficients[I][I] = {{10, -1, 2, 0}, {-1, 11, -1 , 3}, {2, -1, 10, -1}, {0, 3, -1, 8}};
+float variable[I] = {1, 1, 1, 1};
+float independent[I] = {6, 25, -11, 15};
+
+void InitBarrier(int count, pthread_barrier_t* barrier_var){
+    int rc;
+    if (rc = pthread_barrier_init(barrier_var, NULL, count)){
+        printf("Erro ao inicializar barrier\n");
+        exit(1);
+    }
+}
+
+void DestroyBarrier(pthread_barrier_t* barrier_var){
+    pthread_barrier_destroy(barrier_var);
+}
+
+int Scheduler(int* schedule){
+    int remainder = I%N;
+    int partition = I/N;
+    int count = N;
+
+    for (int i = 0; i < I; i++){
+        schedule[i] = partition;
+        if (remainder > 0){ 
+            schedule[i]++;
+            remainder--;
+        }
+    }
+
+    if (!partition) count = I;
+    return count;
+}
+
+void* Jacobi(void* thread_file){
+    thread_data data = *(thread_data*) thread_file;
+    int iter = P;
+    float sum_ax;
+    int pos_init = data.pos_init;
+    int partition = data.partition;
+
+    if (partition){
+        float temp_variable[partition];
+        while (iter > 0){
+            for (int i = 0; i < partition; i++){
+                sum_ax = 0;
+                for (int j = 0; j < I; j++){
+                    if (j != (pos_init + i)) sum_ax += coeficients[pos_init + i][j] * variable[j];
+                }
+                temp_variable[i] = (independent[pos_init + i] - sum_ax) / coeficients[pos_init+i][pos_init+i];
+            }
+            iter--;
+            pthread_barrier_wait(&barrier);
+            for (int i = 0; i < partition; i++){
+                variable[pos_init + i] = temp_variable[i];
+            }
+            pthread_barrier_wait(&barrier);
+        }
+    }
+    pthread_exit(NULL);
+}
+
+int main(){
+    thread_data data_list[N];
+    pthread_t thread_list[N];
+    int schedule[N];
+    int count, rc, jc;
+    int pos = 0;
+    
+    count = Scheduler(schedule);
+    InitBarrier(count, &barrier);
+    
+    for (int i = 0; i < count; i++){
+        data_list[i].partition = schedule[i];
+        data_list[i].pos_init = pos;
+        pos += data_list[i].partition;
+
+        if (rc = pthread_create(&thread_list[i], NULL, Jacobi, (void*) &data_list[i])){
+            printf("Erro na criacao da thread\n");
+            exit(1);
+        }
+
+    }
+
+    for (int i = 0; i < count; i++){
+        jc = pthread_join(thread_list[i], NULL);
+        if (jc){
+            printf("Erro de join na thread\n");
+            exit(1);
+        }
+    }
+
+    for (int i = 0; i < I; i++){
+        printf("x%d: %f; ", i+1, variable[i]);
+    }
+    printf("\n");
+
+    DestroyBarrier(&barrier);
+    pthread_exit(NULL);
+}
